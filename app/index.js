5a9f1c863a0560bf2e7139717b1559e88bb38ba8
Efe Ali Özkesici <efealiozkesici@hotmail.com>
2019-03-24 21:57:17 +0300

    Project format changed.

---
+++ app/index.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import propTypes from 'prop-types';
+import { KeyboardAvoidingView, StyleSheet, ImageBackground } from 'react-native';
+import Login from './screens/Login';
+
+export default class app extends Component{
+    state = {
+        currentScreen : 'login',
+    }
+
+    changeScreen = screenName => () => {
+        this.setState({currentScreen: screenName});
+    }
+
+    userSuccessfullyLoggedIn = (user) => {
+        this.props.login(user);
+    };
+
+    render() {
+        let screenToShow;
+
+        switch(this.state.currentScreen) {
+        case 'login':
+            screenToShow = <Login change={this.changeScreen} success={this.userSuccessfullyLoggedIn}/>;
+            break;
+        /*case 'register':
+            screenToShow = <Register change={this.changeScreen} />;
+            break;
+        case 'forgot':
+            screenToShow = <ForgotPassword change={this.changeScreen}/>;
+            break;*/
+        }
+
+        return (
+        <KeyboardAvoidingView
+            behavior="position"
+            keyboardVerticalOffset={-w(40)}
+            style={styles.container}
+        >
+            <ImageBackground
+            source={this.props.background}
+            style={styles.background}
+            resizeMode="stretch"
+
+            >
+            {screenToShow}
+            </ImageBackground>
+        </KeyboardAvoidingView>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      width: '100%',
+      height: '100%',
+      backgroundColor: '#555',
+    },
+    background: {
+      width: '100%',
+      height: '100%',
+    }
+});
--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    preset: 'react-native',
-    verbose: true,
-    transform: { '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js' },
-    moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
-  };
-  
